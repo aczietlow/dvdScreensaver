b63138f1a68e7151bc47a4893a5c65e2498ea4a5
aczietlow <aczietlow@gmail.com>
2022-07-30 08:33:42 -0400

    In the beginning...

---
+++ dvd.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"golang.org/x/term"
+	"os"
+)
+
+func main() {
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module dvdScreensaver
+
+go 1.18
+
+require (
+	golang.org/x/sys v0.0.0-20220730100132-1609e554cd39 // indirect
+	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
+)
