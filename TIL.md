3a7b0a0065bbe26fc5b9af0ee2ae923a49902b8a
aczietlow <aczietlow@gmail.com>
2022-08-01 20:54:14 -0400

    Creating a terminal with read buffer.
    Don't understand how go registers functions vs methods based on the receiver in the signature.

---
+++ TIL.md
@@ -0,0 +1,15 @@
+## Unrelated to go
+
+### Terminal modes
+
+* Cooked - Allows for preprocessing of data and is lined based input. "cooks" characters before sending it to the program 
+
+* Raw - Every character is sent directly to the program. e.g. vi.
+** Is typical that QUIT characters are still allowed
+
+## TODO
+
+* dig more into io buffers. e.g. io.ReadWriter
+* -> really just explore the entire os package further.
+
+* What is a VT100 terminal?
--- dvd.go
+++ dvd.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"golang.org/x/term"
 	"os"
 )
@@ -10,5 +11,35 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
+	width, height, err := term.GetSize(int(os.Stdin.Fd()))
+
+	if err != nil {
+		panic(err)
+	}
+
+	// poor man's kill switch
+	//c := make([]byte, 1)
+	//os.Stdin.Read(c)
+	//switch c[0] {
+	//case 'q':
+	//	err = term.Restore(int(os.Stdin.Fd()), oldState)
+	//	os.Exit(0)
+	//
+	//}
+
+	terminal := term.NewTerminal(os.Stdin, "")
+	fmt.Println("user name?")
+	line, err := terminal.ReadLine()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("line:%v\n", line)
+
+	fmt.Printf("unix File Descriptor:%v\n", int(os.Stdin.Fd()))
+	fmt.Printf("x:%v,y:%v\n", width, height)
+	fmt.Println("end of terminal")
 }
